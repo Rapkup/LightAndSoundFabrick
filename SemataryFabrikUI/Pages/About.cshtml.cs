f18f93bd2967bd03fcb5bdc014f246e9829e42e0
Nikita Rapkup <svinkapeppa75@GMAIL.COM>
2025-05-05 16:43:07 +0300

    Add About, TechSupportInfo, ContactInfo Update main navbar

---
+++ SemataryFabrikUI/Pages/About.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace SemataryFabrikUI.Pages
+{
+    public class AboutModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
---
+++ SemataryFabrikUI/Pages/TechSupportInfo.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace SemataryFabrikUI.Pages.Shared
+{
+    public class TechSupportInfoModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
